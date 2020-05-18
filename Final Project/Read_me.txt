1744d88b1a60804245d98c453f878b7edcd817ef
OlidHasan32 <60337388+OlidHasan32@users.noreply.github.com>
2020-05-17 23:40:36 -0400

    Add files via upload

---
+++ Final Project/Read_me.txt
@@ -0,0 +1,5 @@
+All Photos are collected from below websites:
+
+https://www.pexels.com/
+https://pixabay.com/
+
---
+++ Final Project/js/main.js
@@ -0,0 +1,30 @@
+
+  /*function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('no')); 
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 1500);
+    }
+    $('[data-scroll]').on('click', scrollToSection);
+
+
+  //   navbar background colorK
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 25) {
+      $(".navbar").css({"background-color": "black", "transition": ".5s", "opacity": "1"});
+    }
+  
+    else{
+      $(".navbar").css({"background-color": "black", "opacity": ".7"}); 
+    }
+    if (scroll > 26) {
+      $(".mynavbar").css({"background-color": "black", "transition": ".7s"});
+    }
+  
+    else{
+      $(".mynavbar").css({"background-color": "black", "opacity": ".7"});  
+    }
+  }) 
+*/
